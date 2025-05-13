1772de23cc5c75fa30b2e360d726e25b414ee9c8
Gonzalo Zelaya <gonzalo.zelaya@outsourcearg.com>
2025-05-13 00:14:47 -0300

    changed override to write method

--- models/stock_move.py
+++ models/stock_move.py
@@ -31,6 +31,7 @@ class StockMove(models.Model):
 
     @api.depends('move_line_ids.quantity', 'move_line_ids.product_uom_id')
     def _compute_quantity(self):
+        _logger.info("Computing quantity")
         for move in self:
             has_tracking_lines = any(line.lot_id or line.lot_name for line in move.move_line_ids)
             if move.initial_purchase_quantity_zero :
@@ -45,3 +46,32 @@ class StockMove(models.Model):
             else:
                 # Comportamiento normal
                 super(StockMove, move)._compute_quantity()
+
+
+class StockMoveLine(models.Model):
+    _inherit = 'stock.move.line'
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        # Primero modificamos los vals_list según nuestra necesidad
+        for vals in vals_list:
+            if vals.get('move_id'):
+                move = self.env['stock.move'].browse(vals['move_id'])
+                if move.quantity == 0:
+                    _logger.info("Setting quantity to 0 for move line")
+                    vals['quantity'] = 0  # o 'qty_done' según lo que necesites
+        
+        # Llamamos al super() para que se ejecute la lógica original
+        mls = super().create(vals_list)
+        
+        return mls
+    def write(self, vals):
+        # Verificar si se está modificando quantity y si move_id tiene quantity=0
+        if 'quantity' in vals:
+            for ml in self:
+                if ml.move_id and ml.move_id.quantity == 0:
+                    _logger.info(f"Overriding quantity with product_uom_qty for move line {ml.id}")
+                    vals['quantity'] = ml.move_id.product_uom_qty
+        
+        # Llamar al método original
+        return super().write(vals)
